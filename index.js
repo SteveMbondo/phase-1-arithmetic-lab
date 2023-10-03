89c03bf40b989b040559d400d9a343bf2287c41c
root <+>
2023-10-03 12:32:11 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 =2
+let multiply = num1 * num2
 
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 32
+let num4 = 7
+let mod = num3 % num4
+
+const numList = (1, 3, 8, 15, 20)
+let max = Math.max(numList)
 // Write your code here
